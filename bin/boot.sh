7612bc4a4ed7d0355169720745017a6d21f9cc46
Evan Sarmiento <esarmien@g.harvard.edu>
2018-04-09 12:05:35 -0400

    Re-do boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -3,17 +3,31 @@ for var in `env|cut -f1 -d=`; do
   echo "PassEnv $var" >> /app/apache/etc/apache2/httpd.conf;
 done
 
+psmgr=/tmp/harvardkey-buildpack-wait
+rm -f $psmgr
+mkfifo $psmgr
+
 # cat /app/apache/etc/apache2/httpd.conf
 
 mkdir -p /app/apache/logs
 mkdir -p /app/apache/var/cache
 touch /app/apache/logs/error_log
 touch /app/apache/logs/access_log
-tail -F /app/apache/logs/error_log &
-tail -F /app/apache/logs/access_log &
 
-COMMAND=${@:$n}
-echo "Launching ${COMMAND}..."
+(
+ $COMMAND=${@:$n}
+ echo "Launching ${COMMAND}..."
+ echo 'app' > $psmgr
+) &
+
 $COMMAND &
 echo "Launching apache..."
-exec /app/apache/sbin/httpd -DNO_DETACH
+(
+  echo "Launching apache..."
+  /app/apache/sbin/httpd -DNO_DETACH
+  echo 'apache' > $psmgr
+) & 
+
+read exit_process <$psmgr
+echo "buildpack=apache at=exit process=$exit_process"
+exit 1
